98c0d9aac5b8bb1c953ae91c2c84a89cd6caa440
Pedro Marcelo <pedro.carvalho@hp.com>
2024-04-04 14:58:09 -0300

    feat: sync

--- client1.js
+++ client1.js
@@ -1,7 +1,133 @@
 var PouchDB = require('pouchdb');
+const readLineSync = require('readline-sync');
 
-var db = new PouchDB('http://localhost:4321/dataBase')
 
-var emptyDB = {
-    counter : 0
+var db = new PouchDB('client1DB');
+//var db = new PouchDB('http://localhost:4321/dataBase')
+
+docId = 'doc'
+const emptyDocument = {
+  "_id" : docId,
+  '_rev': 0,
+   "counter": 0
 } 
+
+async function main(){
+    let userRes;
+    while (userRes !== '0') {
+        console.log("CLIENT-1")
+        console.log('########################')
+        console.log("1 - Create Document");
+        console.log("2 - Update counter");
+        console.log("3 - Delete Document");
+        console.log('4 - Read Document');
+        console.log('5 - Reset Counter');
+        console.log('0 - Exit')
+        console.log('########################')
+
+        userRes = readLineSync.question('Choose An Option: ');
+        switch (userRes) {
+            case('1'):
+                await createDoc(emptyDocument);
+            break;
+            case('2'):
+                var doc = await getDoc(docId);
+                if(doc){
+                    let counter = doc['counter'];
+                    doc['counter'] = counter + 1;
+                    await updateDoc(docId, doc);
+                    let res= await getDoc(docId)
+                    console.log(`Counter: ${res['counter']}`);
+                }
+                //console.log(counter);
+            break;
+            case('3'):
+                await delDoc(docId);
+            break;
+            case('4'):
+                let response = await getDoc(docId)
+                console.log(response);
+            break;
+            case('5'):
+            var doc = await getDoc(docId);
+            doc['counter'] = '0';
+            await updateDoc(docId, doc);
+            let r = await getDoc(docId)
+            if(r){
+                console.log(`Counter: ${r['counter']}`);
+            }
+            break;
+            case('0'):
+                break;
+            default:
+                console.log('option does not exist');
+                break;
+        }
+    }
+
+}
+
+main()
+
+db.addListener( '',(event) => {
+    console.log(event);
+})
+
+
+async function getDoc(doc_id){
+    try{
+        var response = await db.get(doc_id);
+        return response
+    }
+    catch(err){
+        console.log(`Error Name: ${err.name}`);
+        console.log(`Error Message: ${err.message}` );
+        console.log(err.status);
+    }
+    
+}
+
+async function createDoc(document){
+    try {
+        console.log('Creating Document...');
+        var response = await db.put(document);
+        console.log(`Document Created: ${response.ok}`)
+      } catch (err) {
+        console.log(`Error Name: ${err.name}`);
+        console.log(`Error Message: ${err.message}` );
+        console.log(err.status);
+      }
+}
+
+async function updateDoc(doc_id, docContents){
+    try{
+        console.log('Updating Document...');
+        var doc = await db.get(doc_id);
+        var response = await db.put({
+            ...docContents,
+            '_id': doc._id,
+            '_rev': doc._rev,
+        })
+        console.log(`Document Updated: ${response.ok}`)
+    }
+    catch(err){
+        console.log(`Error Name: ${err.name}`);
+        console.log(`Error Message: ${err.message}` );
+        console.log(err.status);
+    }
+}
+
+async function delDoc(doc_id){
+    try {
+        console.log('Deleting Document...');
+        var doc = await db.get(doc_id);
+        var response = await db.remove(doc);
+        console.log(`Document Deleted: ${response.ok}`);
+      } catch (err) {
+        console.log(`Error Name: ${err.name}`);
+        console.log(`Error Message: ${err.message}` );
+        console.log(err.status);
+      }
+}
+
+
--- client2.js
+++ client2.js
@@ -1,26 +1,73 @@
 var PouchDB = require('pouchdb');
-const readLineSync = require('readline-sync');
+const readLineSync = require('readline');
+
+const readline = require('readline');
+//const fileStream = fs.createReadStream('input.txt');
+
+const rl = readline.createInterface({
+  input: process.stdin, //or fileStream 
+  output: process.stdout
+});
 
 
-var db = new PouchDB('client2DB');
-//var db = new PouchDB('http://localhost:4321/dataBase')
-
+dbName = 'client2DB'
 docId = 'doc'
 const emptyDocument = {
-  "_id" : docId,
-  '_rev': 0,
-   "counter": 0
-} 
+    "_id" : docId,
+    '_rev': 0,
+     "counter": 0
+  } 
+  
+var db = new PouchDB(dbName);
+//var db = new PouchDB('http://localhost:4321/dataBase')
+//db.changes()
+
+sync()
+
+function sync(){
+    var sync = PouchDB.sync(`${dbName}`, `./client1DB`, {
+        live: true,
+        retry: true
+    }).on('change', function (info) {
+        // handle change
+        console.log(info.direction);
+    }).on('paused', function (err) {
+        console.log('Replication Paused', err);
+        // replication paused (e.g. replication up to date, user went offline)
+    }).on('active', function () {
+        console.log('replicate resumed')
+        // replicate resumed (e.g. new changes replicating, user went back online)
+    }).on('denied', function (err) {
+        console.log(`Replication Denied: ${err}`);
+        // a document failed to replicate (e.g. due to permissions)
+    }).on('complete', function (info) {
+        console.log('Sync Complete')
+    }).on('error', function (err) {
+        console.log(err)
+        // handle error
+    });
+}
+
+
+
 
 async function main(){
     let userRes;
     while (userRes !== '0') {
-        console.log("create document 1");
-        console.log("Update Document 2");
-        console.log("Delete Document 3");
-        console.log('Read Document   4');
-
-        userRes = readLineSync.question('Choose An Option: ');
+        
+        console.log("CLIENT-2")
+        console.log('########################')
+        console.log("1 - Create Document");
+        console.log("2 - Update counter");
+        console.log("3 - Delete Document");
+        console.log('4 - Read Document');
+        console.log('5 - Reset Counter');
+        console.log('0 - Exit')
+        console.log('########################')
+        const userRes = await new Promise(resolve => {
+            rl.question("Choose An Option: ", resolve)
+          })
+        //userRes = readLineSync.question('Choose An Option: ');
         switch (userRes) {
             case('1'):
                 await createDoc(emptyDocument);
@@ -31,14 +78,33 @@ async function main(){
                 //console.log(counter);
                 doc['counter'] = counter + 1;
                 await updateDoc(docId, doc);
+                let res= await getDoc(docId)
+                console.log(`Counter: ${res['counter']}`);
             break;
             case('3'):
                 await delDoc(docId);
             break;
             case('4'):
                 let response = await getDoc(docId)
-                console.log(response);
+                if(response){
+                     console.log(response['counter']);
+                }
+               
             break;
+            case('5'):
+            var doc = await getDoc(docId);
+            doc['counter'] = '0';
+            await updateDoc(docId, doc);
+            let r = await getDoc(docId)
+            console.log(`Counter: ${r['counter']}`);
+            break;
+            case('0'):
+                db.close()
+                //sync.cancel()
+                break;
+            default:
+                console.log('option does not exist');
+                break;
         }
     }
 
@@ -52,8 +118,8 @@ async function getDoc(doc_id){
         return response
     }
     catch(err){
-        console.log(err.name);
-        console.log(err.message);
+        console.log(`Error Name: ${err.name}`);
+        console.log(`Error Message: ${err.message}` );
         console.log(err.status);
     }
     
@@ -63,7 +129,7 @@ async function createDoc(document){
     try {
         console.log('Creating Document...');
         var response = await db.put(document);
-        console.log(`Document Created: ${response.ok}\n`)
+        console.log(`Document Created: ${response.ok}`)
       } catch (err) {
         console.log(err.name);
         console.log(err.message);
@@ -80,7 +146,7 @@ async function updateDoc(doc_id, docContents){
             '_id': doc._id,
             '_rev': doc._rev,
         })
-        console.log(`Document Updated: ${response.ok}\n`)
+        console.log(`Document Updated: ${response.ok}`)
     }
     catch(err){
         console.log(err.name);
@@ -94,12 +160,15 @@ async function delDoc(doc_id){
         console.log('Deleting Document...');
         var doc = await db.get(doc_id);
         var response = await db.remove(doc);
-        console.log(`Document Deleted: ${response.ok}\n`);
+        console.log(`Document Deleted: ${response.ok}`);
       } catch (err) {
         console.log(err.name);
         console.log(err.message);
         console.log(err.status);
       }
 }
+
+
+
 
 
